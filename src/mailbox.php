4e0f4cddcbff22f0780ed5615928f719854a60da
Benedikt Carda <carda@two-wings.net>
2021-10-09 12:07:38 +0200

    First test for synchronization.

--- src/mailbox.php
+++ src/mailbox.php
@@ -11,12 +11,23 @@ class mailbox {
         $this->rcube_imap_generic = $rcube_imap_generic;
         $this->mailboxname = $mailboxname;
         
+        $this->rcube_imap_generic->select($mailboxname);
+        
     }
     
     public function getName() {
         return $this->mailboxname;
     }
 
+    public function getMessagehigherthan($lastfetcheduid) {
+        
+        $nextuid = $lastfetcheduid + 1;
+        $messages = $this->getMessageSequence("$nextuid" . ":*");
+        
+        return $messages;
+        
+    }
+    
     public function getMessageSequence($message_set) {
     
         $result = $this->rcube_imap_generic->fetchHeaders($this->mailboxname, $message_set, true, false, ['message-id', 'uid', 'references']);
@@ -33,6 +44,62 @@ class mailbox {
         
         return $resultarray;
         
+    }
+    
+    public function synchronize($stored_highestmodseq, $stored_uidvalidity) {
+        
+        $qresync   = $this->rcube_imap_generic->get_capability('QRESYNC');
+        $condstore = $qresync ? true : $this->rcube_imap_generic->get_capability('CONDSTORE');
+        
+        $uidvalidity = $this->getStatus->uidvalidity;
+        
+        $returnarray = array();
+        
+        if (!$qresync && !$condstore) {
+            
+            $returnarray["statusmessage"] = 'Mailbox does not support synchronization';
+            $returnarray["status"] = 0;
+            
+        } elseif ($uidvalidity != $stored_uidvalidity) {
+            
+            $returnarray["statusmessage"] = 'UID validity changed, cannot synchronize';
+            $returnarray["status"] = 0;
+            
+        } elseif (empty($stored_highestmodseq)) {
+            
+            $returnarray["statusmessage"] = 'No stored highestmodseq, cannot synchronize';
+            $returnarray["status"] = 0;
+            
+        } else {
+
+            // Enable Qresync
+            $enable_result = $this->rcube_imap_generic->enable($qresync ? 'QRESYNC' : 'CONDSTORE');
+            
+            if ($enable_result === false) {
+                
+                $returnarray["statusmessage"] = 'Could not enable QRESYNC in connection to mailbox';
+                $returnarray["status"] = 0;
+                
+            } else {
+
+                // Close mailbox if already selected to get most recent data                
+                if ($this->rcube_imap_generic->selected == $this->mailboxname) {
+                    $this->rcube_imap_generic->close();
+                }
+                
+                $this->rcube_imap_generic->select($this->mailboxname);
+                
+                $returnarray = $this->rcube_imap_generic->data;
+                
+            }
+            
+            $uids    = [];
+            $removed = [];
+            
+            
+        }
+        
+        return $returnarray;
     }
     
     public function getStatus() {
@@ -65,10 +132,13 @@ class mailbox {
         }
 
     }
-    
-    
 
-    
-    
+    public function getData() {
         
+        $data = $this->rcube_imap_generic->data;
+        
+        return $data;
+        
+    }
+    
 }
