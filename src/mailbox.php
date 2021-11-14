6964aa02498ad7a553e3034a5aeff95ebe0f0b35
Benedikt Carda <carda@two-wings.net>
2021-11-14 10:22:55 +0100

    Signed-off-by: Benedikt Carda <carda@two-wings.net>

--- src/mailbox.php
+++ src/mailbox.php
@@ -479,7 +479,7 @@ class mailbox {
         
         $result = $this->rcube_imap_generic->countMessages($this->mailboxname);
         
-        if ($result == false) {
+        if ($result === false) {
             throw new \Exception('Counting messages failed.');
         }
         
@@ -500,9 +500,7 @@ class mailbox {
         
         $result = $this->rcube_imap_generic->countRecent($this->mailboxname);
         
-        print "<br>result received: $result";
-        
-        if ($result == false) {
+        if ($result === false) {
             throw new \Exception('Counting messages failed.');
         }
         
@@ -523,7 +521,7 @@ class mailbox {
         
         $result = $this->rcube_imap_generic->countUnseen($this->mailboxname);
         
-        if ($result == false) {
+        if ($result === false) {
             throw new \Exception('Counting messages failed.');
         }
         
